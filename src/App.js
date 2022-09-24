47a57ddf79d84f24d8f94ad08b8d1bd1c3887a92
Nas Baki <nas.baki@holman.com>
2022-09-24 11:42:28 -0400

    Intro to React - Setup app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+  JSX (JavaScript XML): Allows us to use JS to write HTML
+
+  React translates [XML -> JS] -> HTML
+    [] = JSX
+
+  let div = createElement('div')
+  div.attributes.class = 'App';
+  div.appendChild(<header>);
+
+  <header class="MyClass" id=""><body>
+  <asdf attr="asdf">
+*/
+/*
+  <App /> = App();
+*/
+
+/*
+  Props: Data passed from a parent component to a child component
+    - Object of information
+    - Read-only
+
+    {
+      title: 'Home Page',
+      foo: 'bar',
+      num: 1
+      ...
+    }
+*/
+
+function SubTitle(data) {
+  // data.title = 'New Title'; // not allowed
+  let title = data.title;
+  return (
+    <h2>{title.toUpperCase()}</h2>
+  )
+}
+
 function App() {
+  let x = 'abc';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>E-commerce Store - Toys</h1>
+      <SubTitle title="Home Page" foo="bar" num={1} x={x} func={() => {}} />
+      <SubTitle title="About Page" />
+      <SubTitle title="Products" />
     </div>
   );
 }
