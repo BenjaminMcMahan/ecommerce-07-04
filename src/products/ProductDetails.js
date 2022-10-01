938bf5a196d15f2c8d3c1f18219b4090ac7ba655
Benjamin McMahan <benjamin@linkedresponder.com>
2022-10-01 18:05:45 -0400

    Wraps JSX in parentheses

--- src/products/ProductDetails.js
+++ src/products/ProductDetails.js
@@ -6,14 +6,16 @@ function ProductDetails() {
   const params = useParams();
 
   console.log(params);
-  return <Card>
-    <CardHeader>Product Details</CardHeader>
-    <CardBody>
-      You are viewing details for product: {params.name}
-      <br />
-      There are {params.quantity} left in stock.
-    </CardBody>
-  </Card>
-};
+  return (
+      <Card>
+        <CardHeader>Product Details</CardHeader>
+        <CardBody>
+          You are viewing details for product: {params.name}
+          <br />
+          There are {params.quantity} left in stock.
+        </CardBody>
+      </Card>
+  )
+}
 
 export default ProductDetails;
